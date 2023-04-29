5b8b1419cde3405942a5a6e01dfd09ce749bbe17
Athan <athan.codes@gmail.com>
2023-04-30 00:04:04 +0200

    Exercice done by athan-code - 30/04/2023 00:00

--- app/src/main/java/com/example/gestionnairedetaches/MainActivity.kt
+++ app/src/main/java/com/example/gestionnairedetaches/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.gestionnairedetaches
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gestionnairedetaches.ui.theme.GestionnaireDeTachesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +33,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ContenuTaskCompleted()
                 }
             }
         }
     }
 }
-
+// Contenu :
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ContenuTaskCompleted() {
+    StructureTaskCompleted(
+        image = painterResource(id = R.drawable.ic_task_completed),
+        statut = stringResource(R.string.statut_completed),
+        congrat = stringResource(R.string.kind_message)
     )
 }
 
-@Preview(showBackground = true)
+// Structure :
+@Composable
+private fun StructureTaskCompleted(
+    image: Painter,
+    statut: String,
+    congrat: String,
+    modifier: Modifier = Modifier,
+){
+    Column (
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = statut,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom  = 8.dp)
+        )
+        Text(
+            text = congrat,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     GestionnaireDeTachesTheme {
-        Greeting("Android")
+        ContenuTaskCompleted()
     }
 }
